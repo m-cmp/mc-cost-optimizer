57fe92b17dc7051ce4c782d14da9a7de8a6971cc
Jongwon.Jeon <jw6963@naver.com>
2024-04-19 09:43:09 +0900

    slack api error processing

--- AlarmService/src/main/java/com/mcmp/slack_demo/slack/api_client/SlackACService.java
+++ AlarmService/src/main/java/com/mcmp/slack_demo/slack/api_client/SlackACService.java
@@ -4,22 +4,24 @@ import com.mcmp.slack_demo.slack.encryto.TokenService;
 import com.slack.api.Slack;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.naming.AuthenticationException;
 import java.io.IOException;
 import java.util.Map;
 
 @Service
 public class SlackACService {
-    private TokenService tokenService;
+    private final TokenService tokenService;
 
     @Autowired
     public SlackACService(TokenService tokenService) {
         this.tokenService = tokenService;
     }
 
-    public void sendMessage(String userId, String message, String linkUrl, String linkText) {
+    public void sendMessage(String userId, String message, String linkUrl, String linkText) throws SlackApiException, IOException {
         Slack slack = Slack.getInstance();
         String fullMessage = message;
 
@@ -34,11 +36,20 @@ public class SlackACService {
                     .channel(result.get("channel"))
                     .text(fullMessage)
                     .build();
-
-
-            slack.methods(result.get("token")).chatPostMessage(request);
+            ChatPostMessageResponse response = slack.methods(result.get("token")).chatPostMessage(request);
+            if (!response.isOk()) {
+                switch (response.getError()) {
+                    case "invalid_auth":
+                        throw new AuthenticationException("Invalid authentication credentials for Slack.");
+                    case "channel_not_found":
+                        throw new Exception("The specified channel was not found.");
+                    default:
+                        throw new Exception("An error occurred with Slack API: " + response.getError());
+                }
+            }
         } catch (IOException | SlackApiException e) {
             e.printStackTrace();
+            throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
