91f71be02d55459e7a154b3542993ce8bf67ca63
NYujeong <nyj7135@gmail.com>
2024-06-21 17:07:17 +0900

    HOME selectbox/footer commit

--- cost-fe/src/main.js
+++ cost-fe/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 // import store from './store'
 
 // Bootstrap CSS, JavaScript
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createApp(App).use(router).mount('#app')
