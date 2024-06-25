d9484e860341fcc0fac324818735dbf94ee19ab2
NYujeong <nyj7135@gmail.com>
2024-06-25 11:26:06 +0900

    dashboard layout template

--- cost-fe/src/components/pages/dashboard/DashboardLayout.vue
+++ cost-fe/src/components/pages/dashboard/DashboardLayout.vue
@@ -1,13 +1,32 @@
 <template>
 <div class="page">
+    <!-- page header -->
     <DashboardHeader />
     <div class="page-wrapper">
-        
+
         <!-- selectbox -->
         <DashboardSelectbox />
 
-        <!-- body -->
-        
+        <!-- page body -->
+        <div class="page-body">
+            <div class="container-xl">
+                <div class="row row-deck row-cards">
+                    <div class="col-lg-6">
+                        <DashboardBilling />
+                    </div>
+                    <div class="col-lg-6">
+                        <DashboardTop />
+                    </div>
+                    <div class="col-lg-6">
+                    <DashboardAsset />
+                    </div>
+                    <div class="col-lg-6">
+                        <DashboardCommitment />
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <!-- footer -->
         <DashboardFooter />
 
@@ -17,17 +36,26 @@
 
 <script>
 import DashboardHeader from './dashboard-header/DashboardHeader.vue'
-import DashboardSelectbox from './dashboard-header/DashboardSelectbox.vue'
-import DashboardFooter from './dashboard-header/DashboardFooter.vue'
+import DashboardSelectbox from './dashboard-selectbox/DashboardSelectbox.vue'
+import DashboardFooter from './dashboard-footer/DashboardFooter.vue'
+import DashboardBilling from './dashboard-billing-amount/DashboardBilling.vue'
+import DashboardTop from './dashboard-top-resources/DashboardTop5.vue'
+import DashboardAsset from './dashboard-asset/DashboardAsset.vue'
+import DashboardCommitment from './dashboard-commitment/DashboardCommitment.vue'
 
 export default {
     components: {
         DashboardHeader,
         DashboardSelectbox,
-        DashboardFooter
+        DashboardFooter,
+        DashboardBilling,
+        DashboardTop,
+        DashboardAsset,
+        DashboardCommitment
     }
 }
 </script>
 
 <style>
+
 </style>
