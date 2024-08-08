bdf34c3ef32992b4265bf7c9329287da25dad80a
yjeong-hz <nccess7854@naver.com>
2024-08-08 10:47:04 +0900

    temp alarm_dashboard fe commit

--- cost-fe/src/components/pages/userGuide/guideGrid/AlarmGuideGrid.vue
+++ cost-fe/src/components/pages/userGuide/guideGrid/AlarmGuideGrid.vue
@@ -2,14 +2,14 @@
   <div class="col-12">
     <div class="card">
       <div class="card-header">
-        <h3 class="card-title">Invoices</h3>
+        <h3 class="card-title">비용상태 및 알람</h3>
       </div>
       <div class="card-body border-bottom py-3">
         <div class="d-flex">
           <div class="text-muted">
             Show
             <div class="mx-2 d-inline-block">
-              <input type="text" class="form-control form-control-sm" value="8" size="3" aria-label="Invoices count">
+              <input type="text" class="form-control form-control-sm" value="8" size="1" aria-label="Invoices count">
             </div>
             entries
           </div>
@@ -29,12 +29,13 @@
             <th class="w-1">No. <!-- Download SVG icon from http://tabler-icons.io/i/chevron-up -->
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15l6 -6l6 6" /></svg>
             </th>
-            <th>Invoice Subject</th>
-            <th>Client</th>
-            <th>VAT No.</th>
-            <th>Created</th>
-            <th>Status</th>
-            <th>Price</th>
+            <th>이벤트 유형</th>
+            <th>계정명</th>
+            <th>인스턴스 ID</th>
+            <th>인스턴스 유형</th>
+            <th>알림생성일자</th>
+            <th>긴급도</th>
+            <th>비고</th>
             <th></th>
           </tr>
           </thead>
@@ -57,6 +58,7 @@
               <span class="badge bg-success me-1"></span> Paid
             </td>
             <td>$887</td>
+            <td>비고 설명</td>
             <td class="text-end">
                             <span class="dropdown">
                               <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
@@ -89,198 +91,7 @@
               <span class="badge bg-warning me-1"></span> Pending
             </td>
             <td>$1200</td>
-            <td class="text-end">
-                            <span class="dropdown">
-                              <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
-                              <div class="dropdown-menu dropdown-menu-end">
-                                <a class="dropdown-item" href="#">
-                                  Action
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                  Another action
-                                </a>
-                              </div>
-                            </span>
-            </td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-            <td><span class="text-muted">001403</span></td>
-            <td><a href="invoice.html" class="text-reset" tabindex="-1">New Dashboard</a></td>
-            <td>
-              <span class="flag flag-country-us"></span>
-              Bluewolf
-            </td>
-            <td>
-              87952621
-            </td>
-            <td>
-              23 Oct 2017
-            </td>
-            <td>
-              <span class="badge bg-warning me-1"></span> Pending
-            </td>
-            <td>$534</td>
-            <td class="text-end">
-                            <span class="dropdown">
-                              <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
-                              <div class="dropdown-menu dropdown-menu-end">
-                                <a class="dropdown-item" href="#">
-                                  Action
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                  Another action
-                                </a>
-                              </div>
-                            </span>
-            </td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-            <td><span class="text-muted">001404</span></td>
-            <td><a href="invoice.html" class="text-reset" tabindex="-1">Landing Page</a></td>
-            <td>
-              <span class="flag flag-country-us"></span>
-              Salesforce
-            </td>
-            <td>
-              87953421
-            </td>
-            <td>
-              2 Sep 2017
-            </td>
-            <td>
-              <span class="badge bg-secondary me-1"></span> Due in 2 Weeks
-            </td>
-            <td>$1500</td>
-            <td class="text-end">
-                            <span class="dropdown">
-                              <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
-                              <div class="dropdown-menu dropdown-menu-end">
-                                <a class="dropdown-item" href="#">
-                                  Action
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                  Another action
-                                </a>
-                              </div>
-                            </span>
-            </td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-            <td><span class="text-muted">001405</span></td>
-            <td><a href="invoice.html" class="text-reset" tabindex="-1">Marketing Templates</a></td>
-            <td>
-              <span class="flag flag-country-us"></span>
-              Printic
-            </td>
-            <td>
-              87956621
-            </td>
-            <td>
-              29 Jan 2018
-            </td>
-            <td>
-              <span class="badge bg-danger me-1"></span> Paid Today
-            </td>
-            <td>$648</td>
-            <td class="text-end">
-                            <span class="dropdown">
-                              <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
-                              <div class="dropdown-menu dropdown-menu-end">
-                                <a class="dropdown-item" href="#">
-                                  Action
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                  Another action
-                                </a>
-                              </div>
-                            </span>
-            </td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-            <td><span class="text-muted">001406</span></td>
-            <td><a href="invoice.html" class="text-reset" tabindex="-1">Sales Presentation</a></td>
-            <td>
-              <span class="flag flag-country-us"></span>
-              Tabdaq
-            </td>
-            <td>
-              87956621
-            </td>
-            <td>
-              4 Feb 2018
-            </td>
-            <td>
-              <span class="badge bg-secondary me-1"></span> Due in 3 Weeks
-            </td>
-            <td>$300</td>
-            <td class="text-end">
-                            <span class="dropdown">
-                              <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
-                              <div class="dropdown-menu dropdown-menu-end">
-                                <a class="dropdown-item" href="#">
-                                  Action
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                  Another action
-                                </a>
-                              </div>
-                            </span>
-            </td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-            <td><span class="text-muted">001407</span></td>
-            <td><a href="invoice.html" class="text-reset" tabindex="-1">Logo & Print</a></td>
-            <td>
-              <span class="flag flag-country-us"></span>
-              Apple
-            </td>
-            <td>
-              87956621
-            </td>
-            <td>
-              22 Mar 2018
-            </td>
-            <td>
-              <span class="badge bg-success me-1"></span> Paid Today
-            </td>
-            <td>$2500</td>
-            <td class="text-end">
-                            <span class="dropdown">
-                              <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
-                              <div class="dropdown-menu dropdown-menu-end">
-                                <a class="dropdown-item" href="#">
-                                  Action
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                  Another action
-                                </a>
-                              </div>
-                            </span>
-            </td>
-          </tr>
-          <tr>
-            <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-            <td><span class="text-muted">001408</span></td>
-            <td><a href="invoice.html" class="text-reset" tabindex="-1">Icons</a></td>
-            <td>
-              <span class="flag flag-country-us"></span>
-              Tookapic
-            </td>
-            <td>
-              87956621
-            </td>
-            <td>
-              13 May 2018
-            </td>
-            <td>
-              <span class="badge bg-success me-1"></span> Paid Today
-            </td>
-            <td>$940</td>
+            <td>비고 설명</td>
             <td class="text-end">
                             <span class="dropdown">
                               <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
@@ -322,22 +133,157 @@
         </ul>
       </div>
     </div>
+    <div class="test">
+      <div class="card">
+        <DataTable v-model:filters="filters" :value="products" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem">
+          <template #header>
+            <div class="c-card-header">
+              <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+                <span class="text-xl text-900 font-bold">비용상태 및 알람</span>
+              </div>
+              <div class="flex justify-end">
+                <IconField iconPosition="left">
+                  <InputIcon>
+                    <i class="pi pi-search" />
+                  </InputIcon>
+                  <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
+                </IconField>
+              </div>
+            </div>
+          </template>
+          <Column field="이벤트 유형" header="이벤트 유형"></Column>
+          <Column field="name" header="계정명">
+            <template #body="slotProps">
+              {{ formatCurrency(slotProps.data.price) }}
+            </template>
+          </Column>
+          <Column field="code" header="인스턴스 ID" sortable ></Column>
+          <Column field="인스턴스 유형" header="인스턴스 유형">
+            <template #body="slotProps">
+              <Rating :modelValue="slotProps.data.rating" readonly :cancel="false" />
+            </template>
+          </Column>
+          <Column field="price" header="알림생성일자" sortable >
+          </Column>
+          <Column field="inventoryStatus" header="긴급도" sortable >
+            <template #body="slotProps">
+              <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data)" />
+            </template>
+          </Column>
+          <Column header="비고">
+            <template #body="slotProps">
+              <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data)" />
+            </template>
+          </Column>
+          <Column>
+            <template #body="slotProps">
+              <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data)" />
+            </template>
+          </Column>
+          <template #footer> In total there are {{ products ? products.length : 0 }} products. </template>
+        </DataTable>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Tag from 'primevue/tag';
+import Rating from 'primevue/rating';
+import { FilterMatchMode } from 'primevue/api';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
+// import flexStyle from 'primeflex/primeflex.css';
+
 export default {
+  components:{
+    DataTable,
+    Column,
+    Tag,
+    Rating,
+    IconField,
+    InputIcon,
+    InputText
+  },
   props: {
   },
   data() {
     return {
+      filters: {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      },
+      products: [{
+        id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        description: 'Product Description',
+        image: 'bamboo-watch.jpg',
+        price: 65,
+        category: 'Accessories',
+        quantity: 24,
+        inventoryStatus: 'INSTOCK',
+        rating: 5
+      },{
+        id: '2000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        description: 'Product Description',
+        image: 'bamboo-watch.jpg',
+        price: 50,
+        category: 'Accessories',
+        quantity: 24,
+        inventoryStatus: 'TEST',
+        rating: 5
+      }]
     };
   },
+  mounted() {
+
+  },
   methods: {
+    formatCurrency(value) {
+      return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    },
+    getSeverity(product) {
+      switch (product.inventoryStatus) {
+        case 'INSTOCK':
+          return 'success';
+
+        case 'LOWSTOCK':
+          return 'warning';
+
+        case 'OUTOFSTOCK':
+          return 'danger';
+
+        default:
+          return null;
+      }
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+/*.p-datatable .p-datatable-tbody > tr > td {*/
+/*  background-color: black;*/
+/*}*/
+.p-datatable-table {
+  .p-datatable-tbody > tr, td, th {
+    border-color: #e2e8f0 !important;
+    border-style: revert-layer !important;
+    border-width: revert-layer !important;
+  }
+}
+
+.c-card-header{
+  display: flex;
+  justify-content: space-between;
+  width: -webkit-fill-available;
+  align-items: center;
+  padding: 0 24px;
+}
 
 </style>
--- cost-fe/src/main.js
+++ cost-fe/src/main.js
@@ -2,11 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+// import 'primeflex/primeflex.css'
+import 'primevue/resources/themes/aura-light-green/theme.css';
+// import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import PrimeVue from "primevue/config";
 
 // Bootstrap CSS, JavaScript
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App)
+    .use(router)
+    .use(PrimeVue)
+    .use(createPinia())
+    .mount('#app')
 
