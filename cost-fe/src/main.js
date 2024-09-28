9373988d7a6ab11709b253ba5a06eecc2899446d
NYujeong <nyj7135@gmail.com>
2024-09-28 20:29:45 +0900

    Create DataTable from Primevue

--- cost-fe/src/main.js
+++ cost-fe/src/main.js
@@ -2,11 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 // Bootstrap CSS, JavaScript
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+// PrimeVue styles
+import 'primevue/resources/themes/saga-blue/theme.css'  // theme
+import 'primevue/resources/primevue.min.css'  // core CSS
+import 'primeicons/primeicons.css'  // icons
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+const app = createApp(App)
 
+app.use(router).use(createPinia()).use(PrimeVue)
+
+app.mount('#app')
