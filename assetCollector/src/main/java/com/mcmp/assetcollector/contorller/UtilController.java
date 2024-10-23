93952d1e0504c3c0897db6aa4ea9f18d6e63548c
yjeong-hz <nccess7854@naver.com>
2024-10-23 15:39:42 +0900

    fix. healthcheck and docker env

---
+++ assetCollector/src/main/java/com/mcmp/assetcollector/contorller/UtilController.java
@@ -0,0 +1,21 @@
+package com.mcmp.assetcollector.contorller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/costopti/assetclt")
+public class UtilController {
+    @GetMapping("/healthcheck")
+    public ResponseEntity getHealthCheck(){
+        try{
+            return ResponseEntity.ok().body("OK");
+        } catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Alarm Service is not running");
+        }
+    }
+
+}
